ee6ab7c5bcd0e2b6126cfec031c658b789819489
dadang17 <iskandardzulqornain38@gmail.com>
2022-11-26 22:38:15 +0700

    deploy

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
 import sequelize from "sequelize";
 
-const db = new sequelize("auth_db", "root", "", {
-  host: "localhost",
+const db = new sequelize("railway", "root", "7VGvF6EVdlYq84evtoFu", {
+  host: "containers-us-west-109.railway.app",
   dialect: "mysql",
+  port: "5866",
 });
 
 export default db;
