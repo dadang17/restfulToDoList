66a2a797eb60da58818f4dfc4fb944bfe80bcbf2
dadang17 <iskandardzulqornain38@gmail.com>
2022-11-27 21:12:46 +0700

    deploy railway

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const userController = require("../controller/user.controller");
 const verifyToken = require("../middleware/auth.token");
+const { route } = require("./todo.route");
 const router = express.Router();
 
 router.get("/users", verifyToken.verifyToken, userController.getUsers);
 router.get("/users/:id", verifyToken.verifyToken, userController.getUserbyId);
 router.patch("/users/:id", verifyToken.verifyToken, userController.updateUser);
 router.delete("/users/:id", verifyToken.verifyToken, userController.deleteUser);
+router.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 module.exports = router;
