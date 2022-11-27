2bd4c4fee7d18c3fdb90c73f36976cb731c89107
dadang17 <iskandardzulqornain38@gmail.com>
2022-11-27 21:52:17 +0700

    deploy railway

--- routes/todo.route.js
+++ routes/todo.route.js
@@ -3,11 +3,15 @@ const todoController = require("../controller/todo.controller");
 const verifyToken = require("../middleware/auth.token");
 const router = express.Router();
 
-router.post("/todo", todoController.createToDO);
-router.get("/todo", todoController.getToDO);
-router.get("/todo/:id", todoController.getToDobyId);
-router.patch("/todo/:id", todoController.updateToDo);
-router.delete("/todo/:id", todoController.deleteToDo);
-router.get("/todobyuser", todoController.getToDObyUser);
+router.post("/todo", verifyToken.verifyToken, todoController.createToDO);
+router.get("/todo", verifyToken.verifyToken, todoController.getToDO);
+router.get("/todo/:id", verifyToken.verifyToken, todoController.getToDobyId);
+router.patch("/todo/:id", verifyToken.verifyToken, todoController.updateToDo);
+router.delete("/todo/:id", verifyToken.verifyToken, todoController.deleteToDo);
+router.get(
+  "/todobyuser",
+  verifyToken.verifyToken,
+  todoController.getToDObyUser
+);
 
 module.exports = router;
